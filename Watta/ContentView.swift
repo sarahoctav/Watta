f69f3195b3bea847f5d062881cabc0910dbb1f15
sarahoctavv <sarahoctavia@student.ub.ac.id>
2023-05-25 08:47:53 +0700

    test

--- Watta/ContentView.swift
+++ Watta/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "arrow.up")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
