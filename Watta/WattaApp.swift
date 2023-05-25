d825cc5cf8dc146aa679882dfeb912d4c2f34e15
sarahoctavv <sarahoctavia@student.ub.ac.id>
2023-05-25 08:44:04 +0700

    Initial Commit

---
+++ Watta/WattaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WattaApp.swift
+//  Watta
+//
+//  Created by Sarah Uli Octavia on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WattaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
